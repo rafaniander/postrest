f7f0bc5c673393daa8578ad4e6cebe11fabc2eb1
Rafael Marques <rafaelmarques1992@gmail.com>
2017-07-28 15:38:51 -0300

    Commit inicial

---
+++ src/main/java/com/algaworks/Main.java
@@ -0,0 +1,18 @@
+package com.algaworks;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author Rafael Marques
+ */
+@SpringBootApplication
+public class Main {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);        
+    }
+    
+}
